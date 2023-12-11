f867e709b6c097385148fb1b9e96ac38f89503e6
Ricardo <ricardo.dev1991@gmail.com>
2023-12-11 17:17:54 -0300

    Criando nova aplicação em sala de aula.

--- aula11.12/atividadeAula/src/app/app.module.ts
+++ aula11.12/atividadeAula/src/app/app.module.ts
@@ -3,21 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TableComponent } from './table/table.component';
-import { AraraComponent } from './arara/arara.component';
-import { FormsModule } from '@angular/forms';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TableComponent,
-    AraraComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
