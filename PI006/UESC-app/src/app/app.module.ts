57803319815b0b661bd8e8f44926a51368bcd3d5
ricardo <ricardo.dev1991@gmail.com>
2023-12-17 17:51:38 -0300

    ✨ feat(Criando componente de Destaques, Serviços, Resultados e Footer.):

--- PI006/UESC-app/src/app/app.module.ts
+++ PI006/UESC-app/src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuNewsEmphasisComponentComponent } from './menu-news-emphasis-component/menu-news-emphasis-component.component';
 import { HeaderComponentComponent } from './header-component/header-component.component';
+import { NewsComponentComponent } from './news-component/news-component.component';
+import { EmphasisComponentComponent } from './emphasis-component/emphasis-component.component';
+import { ServicesResultsComponentComponent } from './services-results-component/services-results-component.component';
+import { ServiceComponentComponent } from './service-component/service-component.component';
+import { ResultComponentComponent } from './result-component/result-component.component';
+import { FooterComponentComponent } from './footer-component/footer-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuNewsEmphasisComponentComponent,
-    HeaderComponentComponent
+    HeaderComponentComponent,
+    NewsComponentComponent,
+    EmphasisComponentComponent,
+    ServicesResultsComponentComponent,
+    ServiceComponentComponent,
+    ResultComponentComponent,
+    FooterComponentComponent
   ],
   imports: [
     BrowserModule,
